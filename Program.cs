0eb92c9c7210ef0bf7e6d7380290b23cf05e4759
khalin1337 <84382013+khalin1337@users.noreply.github.com>
2023-12-26 18:20:53 +0200

    Task4

--- Program.cs
+++ Program.cs
@@ -101,13 +101,13 @@ namespace Practice_Linq
         {
             //Query 4: Вивести всі матчі збірної Германії з 2018 року по 2020 рік (включно), в яких вона на виїзді програла.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(game => game.Away_team == "Germany" && game.Date.Year >= 2018 && game.Date.Year <= 2020 && game.Home_score > game.Away_score);   // Корегуємо запит !!!
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 4 ========================");
 
-            // див. приклад як має бути виведено:
+            foreach (FootballGame game in selectedGames) { PrintMatch(game); }
 
 
         }
